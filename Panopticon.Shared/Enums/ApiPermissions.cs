96c9ddb8855c2b49f2df09177217aa14a8f7643e
Wren <OtherwiseJunk@gmail.com>
2025-01-03 21:34:15 -0500

    add Read level permission for retreiving balances and nothing else.

--- Panopticon.Shared/Enums/ApiPermissions.cs
+++ Panopticon.Shared/Enums/ApiPermissions.cs
@@ -3,7 +3,8 @@
 [Flags]
 public enum ApiPermissions
 {
-    LibcoinSend = 1 << 0, // API is allowed to send Libcoin from their account.
-    LibcoinGrant = 1 << 1, // API is allowed to create Libcoin directly into user accounts.
-    LibcoinDeduct = 1 << 2, // API is allowed to deduct Libcoin from user accounts.
+    LibcoinRead = 1 << 0,
+    LibcoinSend = 1 << 1, // API is allowed to send Libcoin from their account.
+    LibcoinGrant = 1 << 2, // API is allowed to create Libcoin directly into user accounts.
+    LibcoinDeduct = 1 << 3, // API is allowed to deduct Libcoin from user accounts.
 }
