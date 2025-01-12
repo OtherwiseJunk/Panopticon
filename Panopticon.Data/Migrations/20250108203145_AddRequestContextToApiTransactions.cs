952079ce684d5021f9d39ccf8c4043b8cf3756cd
Wren <OtherwiseJunk@gmail.com>
2025-01-11 19:52:37 -0500

    We don't talk about Bruno no no no no

---
+++ Panopticon.Data/Migrations/20250108203145_AddRequestContextToApiTransactions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Panopticon.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequestContextToApiTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionData",
+                table: "ApiTransactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionData",
+                table: "ApiTransactions");
+        }
+    }
+}
